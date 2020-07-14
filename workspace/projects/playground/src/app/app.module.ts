424ac821ee1bb723bf72fd3b06a08ef0197d8749
hakananit <admin@Hakans-MacBook-Pro.local>
2020-07-15 00:43:50 +0300

    chore(app): import library

--- workspace/projects/playground/src/app/app.module.ts
+++ workspace/projects/playground/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxCookieConsentModule } from 'ngx-cookie-consent';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxCookieConsentModule
   ],
   providers: [],
   bootstrap: [AppComponent]
